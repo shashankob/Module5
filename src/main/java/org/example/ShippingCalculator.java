20dac1e06f5c681f1d762a9d2eee3d2bd8e453ef
shash <obulassk@mail.uc.edu>
2023-06-11 13:47:14 -0400

    First task Commit

---
+++ src/main/java/org/example/ShippingCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ShippingCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the price of item $");
+        double priceItem = scanner.nextDouble();
+
+        double shippingCost;
+        if (priceItem >= 100) {
+            shippingCost = 0;
+        } else {
+            shippingCost = 0.02 * priceItem;
+        }
+
+        double totalPrice = priceItem + shippingCost;
+
+        System.out.println("Your shipping cost is $" + shippingCost + " and your totalPrice is $" + totalPrice);
+    }
+}
